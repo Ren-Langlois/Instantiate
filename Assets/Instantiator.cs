3fcac8c32c04d675e263fa3589b01333b463055a
Ren-Langlois <101406518+Ren-Langlois@users.noreply.github.com>
2022-05-10 11:05:50 -0300

    9/5

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClon;
+    public int NumeroDeCubos;
+    
+
+    public void CloneObjetc()
+    {
+        int contador = 0;
+        while (contador < NumeroDeCubos )
+        {
+            Instantiate(objectToClon);
+            contador++;
+        }
+    }
+}
